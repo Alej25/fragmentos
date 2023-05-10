0964a0792152097bc4df938c1c90da5cd5f4d681
Alej25 <85592431+Alej25@users.noreply.github.com>
2023-05-10 15:24:32 -0600

    primera parte

---
+++ src/main/java/com/example/fragmentos/DataFragment.kt
@@ -0,0 +1,21 @@
+package com.example.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DataFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+                ): View? {
+            // Inflate the layout for this fragment
+           val view:View=    return inflater.inflate(R.layout.fragment_data, container, false);
+                return view
+
+        }
+
+     }
